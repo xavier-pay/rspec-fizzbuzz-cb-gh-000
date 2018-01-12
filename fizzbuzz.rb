e1416460208b874a1e83d698bf674c548ad8134a
Aurelien Xavier Payong D <xavier.payong@gmail.com>
2018-01-12 08:18:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
-def fizzbuzz(n)
-  if n % 3 == 0
-    return "Fizz"
-  end
-end
+fizz_3 = def fizzbuzz(n)
+          if n % 3 == 0
+            return "Fizz"
+          end
+        end
 
 fizzbuzz(3)
